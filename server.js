3c30d354f8647100d0190a4035b8841fd051c77b
Justin Deng <justin.n.deng@gmail.com>
2022-12-11 18:03:41 -0700

    api

--- server.js
+++ server.js
@@ -13,7 +13,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
+////////////////////////////////////////////////
+/*
+	Database connection to a mySQL database
+*/
 
+// const mysql = require('mysql');  //mysql node driver
+
+// if(process.env.JAWSDB_URL)
+// {
+// 	var connection = mysql.createConnection(process.env.JAWSDB_URL)
+// }
+
+// else
+// {
+// 	var connection /*This variable will be used in every query*/ = mysql.createConnection({	
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: 'password',
+// 		database: 'CUThere',
+// 	})
+// }
+
+// connection.connect() 
+
+////////////////////////////////////////////////
 
 
 
@@ -22,12 +46,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')) // forces external files to be inside /public
 
 
-app.get('/',function(req,res)
-{
-	res.render('home')
-})
+////////////////////////////////////////////////
+/*
+	request to home page
+*/
 
 
-app.get('/hello-world', function(req,res){
+
+
+app.get('/', function(req,res){
     res.sendFile('data/hello_world.json',{root: __dirname })
 })
+
+app.get('/close',function(req,res){
+    res.send('closing');
+    server.close()
+
+})
+
+var server = app.listen(port)
+console.log('Server is being hosted on port 3000')
