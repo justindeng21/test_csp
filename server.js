a83fab19d1e6bd5d6a0afc04a2b892759326d4ef
Justin Deng <justin.n.deng@gmail.com>
2022-12-11 18:08:07 -0700

    api

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//server stuff
 const express = require('express');
 const bodyParser = require('body-parser');
 let app = express();
@@ -6,38 +5,6 @@ var port = process.env.PORT || 3000
 app.use(bodyParser.json());
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
-//
-
-
-
-
-
-
-////////////////////////////////////////////////
-/*
-	Database connection to a mySQL database
-*/
-
-// const mysql = require('mysql');  //mysql node driver
-
-// if(process.env.JAWSDB_URL)
-// {
-// 	var connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }
-
-// else
-// {
-// 	var connection /*This variable will be used in every query*/ = mysql.createConnection({	
-// 		host: 'localhost',
-// 		user: 'root',
-// 		password: 'password',
-// 		database: 'CUThere',
-// 	})
-// }
-
-// connection.connect() 
-
-////////////////////////////////////////////////
 
 
 
@@ -55,7 +22,7 @@ app.use(express.static('public')) // forces external files to be inside /public
 
 
 app.get('/', function(req,res){
-    res.sendFile('data/hello_world.json',{root: __dirname })
+    res.sendFile('views/home.html',{root: __dirname })
 })
 
 app.get('/close',function(req,res){
